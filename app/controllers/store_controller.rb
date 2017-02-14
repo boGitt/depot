713cd88c6e7bc8201edde54d01fbbe556da119c1
444137051 <444137051@qq.com>
2017-02-14 11:27:11 +0800

    cart add function

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
   def index
+    session[:counter] = ( session[:counter].nil? ? 0 : session[:counter] + 1 )
+
     @products = Product.order("title asc").all
   end
 end
